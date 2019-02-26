fdfe5ee0c53bb09843bd3c0150ba69d424230f2f
amirsano <amirsano.it@gmail.com>
2019-02-26 07:05:49 +0100

    Neuer Controller

---
+++ src/Controller/TestController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asano5
+ * Date: 25.02.2019
+ * Time: 20:16
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    /**
+     * @Route("/")
+     */
+    public function index() {
+        return new Response("Hi");
+    }
+
+    /**
+     * @Route("/news/{harchi}")
+     */
+    public function show($harchi){
+        return new Response("harchi ".$harchi);
+    }
+
+}
